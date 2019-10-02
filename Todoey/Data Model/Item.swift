4a07990b0ad9c1c11d18664fcb053d47f35cfbd5
Robin <robin-wagner@web.de>
2019-10-02 07:42:12 +0200

    Convert to MVC design pattern

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,16 @@
+//
+//  item.swift
+//  Todoey
+//
+//  Created by Robin Wagner on 30.09.19.
+//  Copyright © 2019 LegendApps. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+  
+  var title: String = ""
+  var done: Bool = false
+  
+}
